05d36d8a14d719eefaa1d6f0bf4dc6e2b9ebdede
Grzegorz Wirtek <grzegorz.wirtek@wp.pl>
2022-03-22 11:18:39 +0100

    Adding .env file

--- app.js
+++ app.js
@@ -4,42 +4,53 @@ const routerLogin = require('./routes/login');
 const routerSudoku = require('./routes/sudoku');
 const path = require('path');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:Entropia1@cluster0.193v2.mongodb.net/sudokuDatabase?retryWrites=true&w=majority');
+console.log('URL MÓJ TO: ', process.env.CONNECTION_URL);
+mongoose.connect(process.env.CONNECTION_URL);
 const db = mongoose.connection;
-db.on('error', (err)=> console.error(error));
-db.once('open', (err)=> console.log('Connected to database'));
+db.on('error', (err) => console.error(error));
+db.once('open', (err) => console.log('Connected to database'));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.get('env');
 
-app.use('/register', routerRegister, express.static(
-  path.join(__dirname, 'views'), {
-    index: 'register.html'
-  })
+app.use(
+	'/register',
+	routerRegister,
+	express.static(path.join(__dirname, 'views'), {
+		index: 'register.html',
+	}),
 );
 
-app.use('/', routerLogin, express.static(
-  path.join(__dirname, 'views'), {
-    index: 'login.html'
-  })
+app.use(
+	'/',
+	routerLogin,
+	express.static(path.join(__dirname, 'views'), {
+		index: 'login.html',
+	}),
 );
 
 let idUser = null;
-routerLogin.post('/:url', async (req, res)=>{
-  idUser = await req.params.url;
-  app.use(`/${idUser}`, routerSudoku, express.static(
-    path.join(__dirname, 'views'), {
-      index: 'sudoku.html'
-    })
-  );
-  res.json({message: 'ok'});
+routerLogin.post('/:url', async (req, res) => {
+	idUser = await req.params.url;
+	app.use(
+		`/${idUser}`,
+		routerSudoku,
+		express.static(path.join(__dirname, 'views'), {
+			index: 'sudoku.html',
+		}),
+	);
+	res.json({ message: 'ok' });
 });
 
 const server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 const server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+app.listen(server_port, server_host, function () {
+	console.log('Listening on port %d', server_port);
 });
